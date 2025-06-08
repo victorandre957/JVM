035c3979c6df71a4ed34c751d612810d620e04ff
Victor André De Moraes <victorandre957@gmail.com>
2025-06-07 21:32:17 -0300

    save files

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,6 @@
 #include "LeitorExibidor.h"
 #include "ClassVisao.h"
+#include <fstream>
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -17,6 +18,27 @@ int main(int argc, char *argv[]) {
     ClassFile *cf = leitor.readClassFile(fp);
     fclose(fp);
 
+    // Gera o nome do arquivo de saída
+    std::string input_name(argv[1]);
+    std::string output_name = input_name + ".resultado.txt";
+    
+    // Abre o arquivo de saída
+    std::ofstream out_file(output_name);
+    if (!out_file) {
+        std::cerr << "Erro ao criar arquivo de saída: " << output_name << std::endl;
+        return 1;
+    }
+    
+    // Redireciona a saída para o arquivo
+    std::streambuf *coutbuf = std::cout.rdbuf(); // salva o buffer original
+    std::cout.rdbuf(out_file.rdbuf()); // redireciona cout para o arquivo
+    
+    // Executa a exibição (agora vai para o arquivo)
     exibeClassFile(cf);
+    
+    // Restaura a saída padrão
+    std::cout.rdbuf(coutbuf);
+    
+    std::cout << "Resultado salvo em: " << output_name << std::endl;
     return 0;
 }
